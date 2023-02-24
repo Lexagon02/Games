59bdc5680c6ecaeffd80c7467ca97b687726d7a6
Lexagon02 <leo.r.bergstrom@gmail.com>
2023-02-24 16:54:24 +0100

    not my games btw

---
+++ SweetCrack/candy.py
@@ -0,0 +1,18 @@
+class Candy:
+    candy_type = "Bean"
+
+def __init__(self, position, type, age):
+    self.position = position
+    self.type = type
+    self.age = age
+
+def getPosition(self):
+    return self.position
+
+def getType(self):
+    return self.type
+
+def getAge(self):
+    return self.age
+
+
---
+++ SweetCrack/main.py
@@ -0,0 +1,14 @@
+import time
+from random import randint 
+import numpy as np
+import candy as c
+
+def main():
+    been = c.Candy(0,"bean", 0)
+    print(been)
+    return 1
+
+
+
+if __name__ == '__main__':
+    main()
